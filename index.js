da5363d27410af5209e1e3d921768c95f20f3731
Arun10092001 <gokularun529@gmail.com>
2026-06-03 16:20:28 +0530

    Fix: Add crypto globally

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("crypto"); // Ensure crypto is available globally for MongoDB driver
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
